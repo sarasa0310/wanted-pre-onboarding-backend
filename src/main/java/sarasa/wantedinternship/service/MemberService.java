95d6108d7c44eaa8e650d44cab1263578a5c38b4
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-08-02 14:28:09 +0900

    Feat: MemberService 구현
    - 이미 가입된 회원인지 검증하는 비즈니스 로직 추가
    - 해당 이메일로 가입된 회원이 이미 존재한다면 RuntimeException을 상속한 커스텀 예외를 던지도록 구현

---
+++ src/main/java/sarasa/wantedinternship/service/MemberService.java
@@ -0,0 +1,31 @@
+package sarasa.wantedinternship.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sarasa.wantedinternship.domain.entity.Member;
+import sarasa.wantedinternship.exception.MemberAlreadyExistsException;
+import sarasa.wantedinternship.repository.MemberRepository;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Long signUp(Member member) {
+        validateAlreadyExistsMember(member);
+
+        Member savedMember = memberRepository.save(member);
+
+        return savedMember.getId();
+    }
+
+    private void validateAlreadyExistsMember(Member member) {
+        if (memberRepository.existsByEmail(member.getEmail())) {
+            throw new MemberAlreadyExistsException("이미 가입된 회원입니다.");
+        }
+    }
+
+}
