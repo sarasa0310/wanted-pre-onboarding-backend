e289bba2f3eb07043e184853f6bf0078adf77af0
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-08-02 09:01:43 +0900

    init

---
+++ src/main/java/sarasa/wantedinternship/WantedInternshipApplication.java
@@ -0,0 +1,13 @@
+package sarasa.wantedinternship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedInternshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedInternshipApplication.class, args);
+    }
+
+}
