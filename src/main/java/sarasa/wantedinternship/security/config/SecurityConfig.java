9e4b0c3504b4e5228e9deaa4be5c491e299ad1d0
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-08-03 14:16:15 +0900

    Feat: JWT 적용을 위한 사전 작업 완료
    - 기본적인 설정을 한 후 SecurityFilterChain을 Bean으로 등록 -> 향후 엔드포인트 보안 설정 예정
    - PasswordEncoder를 Bean으로 등록하여 회원 가입 시 비밀번호를 암호화하여 DB에 저장

---
+++ src/main/java/sarasa/wantedinternship/security/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package sarasa.wantedinternship.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.*;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .csrf(CsrfConfigurer::disable)
+                .formLogin(FormLoginConfigurer::disable)
+                .httpBasic(HttpBasicConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
