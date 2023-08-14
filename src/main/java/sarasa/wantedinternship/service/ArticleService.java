55cde51eeeb9ff10a4e5882e7d5263e0402c043a
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-08-14 22:14:05 +0900

    Refactor: 게시글 캐시 적용
    - 게시글, 게시글 목록 조회 성능을 향상시키기 위해 캐시 적용

--- src/main/java/sarasa/wantedinternship/service/ArticleService.java
+++ src/main/java/sarasa/wantedinternship/service/ArticleService.java
@@ -1,6 +1,8 @@
 package sarasa.wantedinternship.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -22,23 +24,27 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
     private final MemberRepository memberRepository;
 
+    @CacheEvict(value = "article", allEntries = true)
     public Long createArticle(Long memberId, Article article) {
         article.setMember(memberRepository.getReferenceById(memberId));
 
         return articleRepository.save(article).getId();
     }
 
+    @Cacheable("article")
     @Transactional(readOnly = true)
     public Page<Article> findArticles(Pageable pageable) {
         return articleRepository.findAll(pageable);
     }
 
+    @Cacheable("article")
     @Transactional(readOnly = true)
     public Article findOneArticle(Long articleId) {
         return articleRepository.findById(articleId)
                 .orElseThrow(() -> new ArticleNotFoundException("존재하지 않는 게시글입니다."));
     }
 
+    @CacheEvict(value = "article", allEntries = true)
     public Article updateArticle(Long memberId, Long articleId,
                                  ArticleRequest dto) {
         Article article = findOneArticle(articleId);
@@ -53,6 +59,7 @@ public class ArticleService {
         return article;
     }
 
+    @CacheEvict(value = "article", allEntries = true)
     public void deleteArticle(Long memberId, Long articleId) {
         Article article = findOneArticle(articleId);
 
